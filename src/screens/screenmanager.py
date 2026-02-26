49cc5494b488980e47e7ea1061bac6f188037d3c
fujita <t.fujita0715@gmail.com>
2026-02-26 23:20:17 +0900

    screenmanagerを更新

--- src/screens/screenmanager.py
+++ src/screens/screenmanager.py
@@ -1,2 +1,15 @@
 import pyxel
 
+#画面インポート
+from .sampleScreen01 import Screen01
+
+
+#現在mainから01を直接呼んでいます。将来的にここから画面遷移を行いま。
+
+def get_screen(screen_name): # クラスの外に置く
+    if screen_name == "01":
+        return Screen01()
+    elif screen_name == "id":
+        return "dammy"
+    else:
+        return None
