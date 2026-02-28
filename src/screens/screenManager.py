c64d318ebda2e55d723a95eefe98553f8998b05e
fujita <t.fujita0715@gmail.com>
2026-02-28 14:18:13 +0900

    画面遷移修正

--- src/screens/screenManager.py
+++ src/screens/screenManager.py
@@ -6,10 +6,13 @@ from .sampleScreen01 import Screen01
 
 #現在mainから01を直接呼んでいます。将来的にここから画面遷移を行いま。
 
-def get_screen(screen_name): # クラスの外に置く
-    if screen_name == "01":
-        return Screen01()
-    elif screen_name == "id":
-        return "dammy"
-    else:
+# from .settingScreen import SettingScreen 
+
+class ScreenManager:
+    @staticmethod
+    def get_screen(screen_name):
+        if screen_name == "01":
+            return Screen01()
+        # if screen_name == "setting":
+        #     return SettingScreen()
         return None
